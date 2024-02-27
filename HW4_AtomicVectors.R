7f602d996f34099e7d3959d26766d846741a20ef
cikpechu <cikpechu@uvm.edu>
2024-02-27 17:32:38 -0500

    Homework 4

---
+++ HW4_AtomicVectors.R
@@ -0,0 +1,13 @@
+
+## Ques 1: Suppose x = 1.1, a = 2.2, and b = 3.3. Assign each expression to the value of the variable z and print the value stored in z.
+# xab
+# (xa)b
+# 3x3+2x2+1
+# 
+
+x = 1.1
+a = 2.2
+b = 3.3
+
+z = x^a^b
+print(z)
